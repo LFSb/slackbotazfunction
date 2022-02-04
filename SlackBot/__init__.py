60c8d06736c933e5986831044428dcfe9cba6ba7
Lee Beenen <leebeenen@gmail.com>
2022-02-04 15:51:43 +0100

    Add temporary stub function

--- SlackBot/__init__.py
+++ SlackBot/__init__.py
@@ -1,5 +1,6 @@
 import logging
 import os
+from urllib import response
 from slack import WebClient
 from slack.errors import SlackApiError
 
@@ -8,15 +9,17 @@ import azure.functions as func
 def main(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
 
-    client = WebClient(token=os.environ['SLACK_API_TOKEN'])
+    return func.HttpResponse(F"Hello! You're calling something that doesn't actually do anything yet. Come back later to see if anything interesting has happened.")
 
-    try:
-        response = client.chat_postMessage(
-            channel='#random',
-            text="Hello world!")
-        assert response["message"]["text"] == "Hello world!"
-    except SlackApiError as e:
-        # You will get a SlackApiError if "ok" is False
-        assert e.response["ok"] is False
-        assert e.response["error"]  # str like 'invalid_auth', 'channel_not_found'
-        print(f"Got an error: {e.response['error']}")
+    # client = WebClient(token=os.environ['SLACK_API_TOKEN'])
+
+    # try:
+    #     response = client.chat_postMessage(
+    #         channel='#random',
+    #         text="Hello world!")
+    #     assert response["message"]["text"] == "Hello world!"
+    # except SlackApiError as e:
+    #     # You will get a SlackApiError if "ok" is False
+    #     assert e.response["ok"] is False
+    #     assert e.response["error"]  # str like 'invalid_auth', 'channel_not_found'
+    #     print(f"Got an error: {e.response['error']}")
