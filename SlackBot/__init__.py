1fe8efe137a4f6910a188f5075088e36bdfcd7e3
Lee Beenen <leebeenen@gmail.com>
2022-02-04 17:58:59 +0100

    Add parameter message to post to slack, log critical errors

--- SlackBot/__init__.py
+++ SlackBot/__init__.py
@@ -9,15 +9,17 @@ import azure.functions as func
 def main(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
 
+    msg = req.params.get('msg')
+    
     client = WebClient(token=os.environ['SLACK_API_TOKEN'])
 
     try:
         response = client.chat_postMessage(
             channel='#random',
-            text="Hello world!")
-        assert response["message"]["text"] == "Hello world!"
+            text=msg)
+        assert response["message"]["text"] == msg
     except SlackApiError as e:
         # You will get a SlackApiError if "ok" is False
         assert e.response["ok"] is False
         assert e.response["error"]  # str like 'invalid_auth', 'channel_not_found'
-        print(f"Got an error: {e.response['error']}")
+        logging.CRITICAL(f"Got an error: {e.response['error']}")
